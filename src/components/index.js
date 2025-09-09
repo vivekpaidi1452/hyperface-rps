7e2ce73b566a7e01e60a870133a652ca8264db4e
vivekpaidi1452 <vivekpaidi1452@gmail.com>
2025-09-09 10:07:09 +0530

    exports

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// Core components
+export { default as Card } from './Card';
+export { default as Button } from './Button';
+export { default as Navbar } from './Navbar';
+
+// Section components
+export { default as LandingSection } from './LandingSection';
+export { default as FeaturesSection } from './FeaturesSection';
+export { default as HowItWorksSection } from './HowItWorksSection';
+export { default as GameLobby } from './GameLobby';
+export { default as GameWindow } from './GameWindow';
+
+// Common components
+export * from './common';
+
+// Lazy components
+export * from './LazyWrapper';
